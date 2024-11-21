4a896b989eba9e338a6847ff162faba089ce7123
Juanjoloq <juandual2607@gmail.com>
2024-11-21 08:16:23 -0500

    Arreglo de las validaciones

--- static/js/main.js
+++ static/js/main.js
@@ -16,9 +16,6 @@ function validacion() {
     } if (contra === "") {
         document.getElementById("ErrorPassword").innerText = "Este campo es obligatorio.";
         return false;
-    } else if (!/^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(contra)) {
-        document.getElementById("ErrorPassword").innerText = "Ingresa un numero, caracter especial y una mayuscula";
-        return false;
     } else {
         document.getElementById("ErrorPassword").innerText = "";
 
